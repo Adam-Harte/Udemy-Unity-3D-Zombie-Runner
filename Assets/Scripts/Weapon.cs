3bfdb4ff6c21b27f030e77bf0c8f66ca9029a06b
Adam-Harte <adamharte11@hotmail.co.uk>
2024-06-04 22:34:13 +0100

    Enemy TakeDamage logic added

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -8,6 +8,8 @@ public class Weapon : MonoBehaviour
     Camera fpCamera;
     [SerializeField]
     float range = 100f;
+    [SerializeField]
+    float damage = 10f;
     private void Update()
     {
         if (Input.GetButtonDown("Fire1"))
@@ -19,8 +21,17 @@ public class Weapon : MonoBehaviour
     void Shoot()
     {
         RaycastHit hit;
-        Physics.Raycast(fpCamera.transform.position, fpCamera.transform.forward, out hit, range);
 
-        Debug.Log($"we hit : {hit.transform.name}");
+        if (Physics.Raycast(fpCamera.transform.position, fpCamera.transform.forward, out hit, range))
+        {
+            Debug.Log($"we hit : {hit.transform.name}");
+            EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();
+            if (target == null) { return; }
+            target.TakeDamage(damage);
+        }
+        else
+        {
+            return;
+        }
     }
 }
